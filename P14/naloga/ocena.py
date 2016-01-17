033877b9a09662df685a7aa74f930f2e62683180
zigastrgar <ziga_strgar@hotmail.com>
2016-01-17 19:46:17 +0100

    Optimization

--- P14/naloga/ocena.py
+++ P14/naloga/ocena.py
@@ -8,17 +8,19 @@ class Circle:
     def __init__(self):
         self.radius = 10
         self.color = risar.nakljucna_barva()
-        self.x, self.y = random.randint(self.radius + 5, risar.maxX - (self.radius + 5)), random.randint(
-            self.radius + 5, risar.maxY - (self.radius + 5))
+        self.x = random.randint(self.radius + 5, risar.maxX - (self.radius + 5))
+        self.y = random.randint(self.radius + 5, risar.maxY - (self.radius + 5))
         self.angle = random.randint(0, 360)
         self.head = risar.krog(self.x, self.y, self.radius, self.color)
         self.exploded = False
         self.timer = risar.QTimer()
+        self.move_x = random.choice([-5, 5])
+        self.move_y = random.choice([-5, 5])
 
-    def move(self, vx, vy, mouse):
+    def move(self, mouse):
         if not self.exploded:
             angle = math.radians(self.angle)
-            self.x, self.y = self.x + vx * math.cos(angle), self.y - vy * math.sin(angle)
+            self.x, self.y = self.x + self.move_x * math.cos(angle), self.y - self.move_y * math.sin(angle)
             self.update()
         if not mouse.clicked:
             mouse.update()
@@ -27,8 +29,21 @@ class Circle:
                 self.exploded = True
                 self.explode()
 
+    def update(self):
+        if not (self.radius < self.x < risar.maxX - self.radius):
+            self.move_x = -self.move_x
+        if not (self.radius < self.y < risar.maxY - self.radius):
+            self.move_y = -self.move_y
+        self.head.setPos(self.x, self.y)
+
+    def distance(self):
+        for circle in exploded_circles:
+            if math.sqrt((self.x - circle.x) ** 2 + (self.y - circle.y) ** 2) < 40:
+                return True
+
     def explode(self):
         self.head.setRect(-30, -30, 60, 60)
+        self.fill()
         if self not in exploded_circles:
             exploded_circles.append(self)
         self.timer.singleShot(4000, self.del_item)
@@ -38,20 +53,13 @@ class Circle:
         risar.odstrani(self.head)
         del_from_list(self)
 
-    def update(self):
-        if not (self.radius < self.x < risar.maxX - self.radius):
-            move_x[circles.index(self)] = -move_x[circles.index(self)]
-        if not (self.radius < self.y < risar.maxY - self.radius):
-            move_y[circles.index(self)] = -move_y[circles.index(self)]
-        self.head.setPos(self.x, self.y)
-
-    def distance(self):
-        for circ in exploded_circles:
-            if math.sqrt((self.x - circ.x) ** 2 + (self.y - circ.y) ** 2) < 40:
-                return True
+    def fill(self):
+        c = self.head.pen().color().lighter()
+        c.setAlpha(192)
+        self.head.setBrush(c)
 
 
-class Mouse(Circle):
+class Mouse(Circle):  # Mouse inherits from Circle function del_item so there is no function duplication
     def __init__(self):
         self.x, self.y = risar.miska
         self.head = risar.krog(self.x, self.y, 30)
@@ -64,6 +72,7 @@ class Mouse(Circle):
             self.head.setPos(self.x, self.y)
         else:
             self.clicked = True
+            self.fill()
             if self not in exploded_circles:
                 exploded_circles.append(self)
             self.timer.singleShot(4000, self.del_item)
@@ -75,10 +84,7 @@ def del_from_list(circle):
 
 def del_from_circles(circle):
     counter.append(1)
-    ind = circles.index(circle)
-    del circles[ind]
-    del move_x[ind]
-    del move_y[ind]
+    del circles[circles.index(circle)]
 
 
 game = 1
@@ -88,8 +94,6 @@ while game <= 5:
     balls = game * per_level + 4
     req = req_per_level * game + 1
     circles = []
-    move_x = []
-    move_y = []
     m = Mouse()
     exploded_circles = []
     counter = []
@@ -98,14 +102,12 @@ while game <= 5:
 
     for fn in range(balls):
         circles.append(Circle())
-        move_x.append(random.choice([-5, 5]))
-        move_y.append(random.choice([-5, 5]))
 
     while not m.clicked or len(exploded_circles) > 0:
         i = 0
         while len(circles) > i:
-            circle = circles[i]
-            circle.move(move_x[i], move_y[i], m)
+            circ = circles[i]
+            circ.move(m)
             i += 1
         risar.cakaj(0.02)
 
@@ -119,6 +121,5 @@ while game <= 5:
             risar.QMessageBox.information(None, "Konec",
                                           "Žal ti ni useplo narediti levela {}. Razpočil si {} žogic od potrebnih {}.".format(
                                                   game, number, req))
-            game = game
     risar.pobrisi()
     risar.klik = False
